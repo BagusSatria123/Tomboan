86aa01741ead9153cb51ead981295ba086215fa0
BagusSatria123 <satriabagusputra123@gmail.com>
2021-03-20 22:35:50 +0700

    footer done saran done tumbuhan done suguhan done

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="row">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d987.5537865480212!2d112.70407082662858!3d-8.07952742637345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd621116381c717%3A0x154834ba35e6ce6b!2sTomboan%20Ngawonggo!5e0!3m2!1sid!2sid!4v1616076665028!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe> <iframe src="" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d987.5537865480212!2d112.70407082662858!3d-8.07952742637345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd621116381c717%3A0x154834ba35e6ce6b!2sTomboan%20Ngawonggo!5e0!3m2!1sid!2sid!4v1616076665028!5m2!1sid!2sid" width="1550" height="750" style="border:0;" allowfullscreen="" loading="lazy"></iframe> <iframe src="" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 </div>
 <footer class="site-footer">
--- suguhan.php
+++ suguhan.php
@@ -1,81 +1,83 @@
 <?php
 if (!isset($_GET['jenis'])) {
 ?>
-<div class="site-blocks-cover overlay" style="background-image: url(images/suguhan1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
-	<div class="container">
-		<div class="row align-items-center justify-content-center">
-			<div class="col-md-7 text-center" data-aos="fade">
-				<h1 class="mb-4">Suguhan</h1>
-                <p class="mb-4">Suguhan Kami Tidak Mengandung Hewan</p>
+	<div class="site-blocks-cover overlay" style="background-image: url(images/suguhan1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+		<div class="container">
+			<div class="row align-items-center justify-content-center">
+				<div class="col-md-7 text-center" data-aos="fade">
+					<h1 class="mb-4">Suguhan</h1>
+					<p class="mb-4">Suguhan Kami Tidak Mengandung Hewan</p>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-<div class="site-section bg-light">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 mx-auto text-center mb-4 section-heading">
-				<h2 class="mb-5">Suguhan</h2>
+	<div class="site-section bg-light">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 mx-auto text-center mb-4 section-heading">
+					<h2 class="mb-5">Suguhan</h2>
+				</div>
+			</div>
+			<div class="row">
+				<?php
+				$sql = mysqli_query($conn, "SELECT * FROM suguhan");
+				foreach ($sql as $value) {
+				?>
+					<div class="col-md-6 col-lg-4 mb-5">
+						<div class="hotel-room text-center">
+							<a href="?page=suguhan&jenis=<?= $value['jenis']; ?>" class="d-block mb-0 thumbnail"><img src="images/<?= $value['gambar']; ?>" alt="Image" class="img-fluid"></a>
+							<div class="hotel-room-body">
+								<h3 class="heading mb-0"><a href="#"><?= $value['jenis']; ?></a></h3>
+								<strong class="price"><?= $value['deskripsi']; ?></strong>
+							</div>
+						</div>
+					</div>
+				<?php
+				}
+				?>
 			</div>
 		</div>
-		<div class="row">
-			<?php
-			$sql=mysqli_query($conn,"SELECT * FROM suguhan");
-			foreach ($sql as $value) {
-			?>
-			<div class="col-md-6 col-lg-4 mb-5">
-				<div class="hotel-room text-center">
-					<a href="?page=suguhan&jenis=<?= $value['jenis']; ?>" class="d-block mb-0 thumbnail"><img src="images/<?= $value['gambar']; ?>" alt="Image" class="img-fluid"></a>
-					<div class="hotel-room-body">
-						<h3 class="heading mb-0"><a href="#"><?= $value['jenis']; ?></a></h3>
-						<strong class="price"><?= $value['deskripsi']; ?></strong>
+	<?php
+} else {
+	?>
+		<div class="site-blocks-cover overlay" style="background-image: url(images/suguhan1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+			<div class="container">
+				<div class="row align-items-center justify-content-center">
+					<div class="col-md-7 text-center" data-aos="fade">
+						<h1 class="mb-4">Suguhan</h1>
 					</div>
 				</div>
 			</div>
-			<?php
-			}
-			?>
 		</div>
-</div>
-<?php
-}else {
-?>
-<div class="site-blocks-cover overlay" style="background-image: url(images/suguhan1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
-	<div class="container">
-		<div class="row align-items-center justify-content-center">
-			<div class="col-md-7 text-center" data-aos="fade">
-				<h1 class="mb-4">Suguhan</h1>
-			</div>
-		</div>
-	</div>
-</div>
-<div class="site-section bg-light">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 mx-auto text-center mb-5 section-heading">
-				<h2 class="mb-5">Suguhan</h2>
-			</div>
-		</div>
-		<div class="row">
-			<?php
-			$sql=mysqli_query($conn,"SELECT * FROM suguhan WHERE Jenis='$_GET[jenis]'");
-			foreach ($sql as $value) {
-			?>
-			<div class="col-md-12 col-lg-12 mb-12">
-				<div class="hotel-room text-center">
-					<a href="?page=ffaa" class="d-block mb-0 thumbnail"><img src="images/<?= $value['gambar']; ?>" alt="Image" class="img-fluid"></a>
-					<div class="hotel-room-body">
-						<h3 class="heading mb-0"><a href="#"><?= $value['jenis']; ?></a></h3>
-						<strong class="price"><?= $value['deskripsi']; ?></strong>
+		<div class="site-section bg-light">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 mx-auto text-center mb-5 section-heading">
+						<h2 class="mb-5">Suguhan</h2>
 					</div>
 				</div>
+				<div class="row">
+					<?php
+					$sql = mysqli_query($conn, "SELECT * FROM suguhan WHERE Jenis='$_GET[jenis]'");
+					foreach ($sql as $value) {
+					?>
+						<div class="col-md-12 col-lg-12 mb-12">
+							<div class="hotel-room text-center">
+								<a class="d-block mb-0 thumbnail"><img src="images/<?= $value['gambar']; ?>" alt="Image" class="img-fluid"></a>
+								<!-- <a href="?page=ffaa" class="d-block mb-0 thumbnail"><img src="images/<?= $value['gambar']; ?>" alt="Image" class="img-fluid"></a> -->
+
+								<div class="hotel-room-body">
+									<h3 class="heading mb-0"><a href="#"><?= $value['jenis']; ?></a></h3>
+									<strong class="price"><?= $value['deskripsi']; ?></strong>
+								</div>
+							</div>
+						</div>
+					<?php
+					}
+					?>
+				</div>
 			</div>
-			<?php
-			}
-			?>
 		</div>
-	</div>
-</div>
-<?php
+	<?php
 }
-?>
+	?>
